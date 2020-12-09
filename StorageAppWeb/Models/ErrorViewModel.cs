9e69194cb462be790735468a40f4020b6d58cfdf
Jenei David <jenei.david1994@gmail.com>
2020-12-09 08:09:32 +0100

    initial commit

---
+++ StorageAppWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StorageAppWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
